243b8647da9a4a2f57381b06910a4969f320319b
faizan2700 <syedfaizan824@gmail.com>
2024-05-31 10:42:04 +0530

    added environment variable settings

--- ecommerce/productservice/models.py
+++ ecommerce/productservice/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from ecommerce.basemodels import TimeStampedModel 
+
 class Product(TimeStampedModel): 
     name = models.CharField(max_length=100) 
     description = models.CharField(max_length=100) 
