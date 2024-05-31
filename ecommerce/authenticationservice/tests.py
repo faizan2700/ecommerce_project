a1edaf4d1d832aa2d0d9fb727b2fcd0c7cd44715
faizan2700 <syedfaizan824@gmail.com>
2024-05-31 10:30:30 +0530

    added tests

--- ecommerce/authenticationservice/tests.py
+++ ecommerce/authenticationservice/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+from authenticationservice.models import User 
+
+class TestUser(TestCase): 
+    def test_user_creation(self): 
+        user = User.objects.create(username='faizan') 
+        self.assertEqual(User.objects.all().count(), 1) 
