0a90607771bddbfeb80a7300c4e17996243f4698
faizan2700 <syedfaizan824@gmail.com>
2024-05-31 10:34:14 +0530

    updated tests

--- ecommerce/authenticationservice/tests.py
+++ ecommerce/authenticationservice/tests.py
@@ -2,10 +2,10 @@ from django.test import TestCase
 
 from authenticationservice.models import User 
 
-class TestUser(TestCase): 
-    # def test_user_creation(self): 
-    #     user = User.objects.create(username='faizan') 
-    #     self.assertEqual(User.objects.all().count(), 1) 
+# class TestUser(TestCase): 
+#     def test_user_creation(self): 
+#         user = User.objects.create(username='faizan') 
+#         self.assertEqual(User.objects.all().count(), 1) 
 
-    def test_one(self): 
-        self.assertEqual(1, 2-1) 
+#     def test_one(self): 
+#         self.assertEqual(1, 2-1) 
